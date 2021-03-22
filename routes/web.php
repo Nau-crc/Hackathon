17eed6c5604a8b237e37af623c1e901e31cad437
Laura <laura.rodcarrrion@gmail.com>
2021-03-22 23:20:46 +0100

    discard admin LTE

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/adminDashboard', function () {
+    return view('admin.dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
